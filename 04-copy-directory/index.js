4feeb030bcf93e143bbf7633b3e56631cb0362fc
Ekaterina1994 <katishteno@mail.ru>
2022-11-07 18:36:57 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const PATH_TO_FOLDER = path.join(__dirname, './files');
+const PATH_TO_COPY = path.join(__dirname, './files-copy');
+
+fs.mkdir(PATH_TO_COPY, { recursive: true }, () => {
+  fs.readdir(PATH_TO_COPY, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        fs.unlink(path.join(__dirname, './files-copy/', file), err => {
+          if (err) return console.error(err);
+        });
+      });
+    }
+  });
+  
+  fs.readdir(PATH_TO_FOLDER, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(__dirname, './files', file.name), path.join(__dirname, './files-copy', file.name), (err) =>
+          {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+
+
+
